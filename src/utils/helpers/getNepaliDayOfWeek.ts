f9a1a64dd13640c924bbe76ec7a1149683fd5c93
anix001 <chaudharyanish305@gmail.com>
2023-03-27 19:59:35 +0545

    Feat: Date Converter BS to AD and viceversa

---
+++ src/utils/helpers/getNepaliDayOfWeek.ts
@@ -0,0 +1,33 @@
+export const getNepaliDayOfWeek = (dayInNumber: any) => {
+    let day;
+    switch (dayInNumber) {
+      case 1:
+        day = "आइतवार";
+        break;
+  
+      case 2:
+        day = "सोमवार";
+        break;
+  
+      case 3:
+        day = "मंगलवार";
+        break;
+  
+      case 4:
+        day = "बुधवार";
+        break;
+  
+      case 5:
+        day = "बिहिवार";
+        break;
+  
+      case 6:
+        day = "शुक्रवार";
+        break;
+  
+      case 7:
+        day = "शनिबार";
+        break;
+    }
+    return day;
+  };
